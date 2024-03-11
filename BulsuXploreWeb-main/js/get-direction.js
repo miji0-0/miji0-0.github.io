b489b6edf0f76b690528bab93542d688cda5b5f3
miji0-0 <109023913+miji0-0@users.noreply.github.com>
2024-03-12 03:57:37 +0800

    Delete BulsuXploreWeb-main directory

--- BulsuXploreWeb-main/js/get-direction.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('route-button').addEventListener('click', function() {
-    var originName = document.getElementById('origin-input').value;
-    var destinationName = document.getElementById('destination-input').value;
-    var origin = getPOICoordinates(originName);
-    var destination = getPOICoordinates(destinationName);
-    fetchValhallaRoute(origin, destination);
-});
